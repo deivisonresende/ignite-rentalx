c000c764093d429f7e53cd25217b29527281776b
Deivison Resende <deivison4844@gmail.com>
2021-09-24 21:47:07 -0300

    Add create users test

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,26 @@
+import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
+import { User } from "@modules/accounts/infra/typeorm/entities/User";
+import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
+
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Create users", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+  it("should be able to create a new user", async () => {
+    const user: ICreateUserDTO = {
+      name: "User test",
+      driver_license: "0123456789",
+      email: "user.test@teste.com",
+      password: "124",
+    };
+    await createUserUseCase.execute(user);
+    const userCreated = await usersRepositoryInMemory.findByEmail(user.email);
+    expect(userCreated.email).toEqual(userCreated.email);
+  });
+});
