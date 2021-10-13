61028f167aab3ef62c81f342c4e815c59e649025
Deivison Resende <deivison4844@gmail.com>
2021-10-13 01:08:03 -0300

    Added UploadCarImageController controller

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
@@ -1,1 +1,25 @@
-export class UploadCarImageUseCase {}
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { UploadCarImageUseCase } from "./UploadCarImageUseCase";
+
+interface IFiles {
+  fileName: string;
+}
+
+export class UploadCarImageController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { car_id } = request.params;
+    const images = request.files as unknown as IFiles[];
+    const uploadCarImageUseCase = container.resolve(UploadCarImageUseCase);
+
+    const images_name = images.map((file) => file.fileName);
+
+    await uploadCarImageUseCase.execute({
+      car_id,
+      images_name,
+    });
+
+    return response.status(201).send();
+  }
+}
