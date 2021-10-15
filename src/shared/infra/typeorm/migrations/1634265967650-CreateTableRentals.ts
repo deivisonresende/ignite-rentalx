333b3778e2702496f031e9e3d0fade823006c319
Deivison Resende <deivison4844@gmail.com>
2021-10-15 00:02:06 -0300

    Refactored create table rentals migration

--- src/shared/infra/typeorm/migrations/1634265967650-CreateTableRentals.ts
+++ src/shared/infra/typeorm/migrations/1634265967650-CreateTableRentals.ts
@@ -1,133 +1,73 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-
-
 export class CreateTableRentals1634265967650 implements MigrationInterface {
-
   public async up(queryRunner: QueryRunner): Promise<void> {
-
     await queryRunner.createTable(
-
       new Table({
-
         name: "rentals",
 
-
-
         columns: [
-
           {
-
             name: "id",
 
-
-
             type: "uuid",
-
-
 
             isPrimary: true,
-
           },
 
-
-
           {
-
             name: "car_id",
 
-
-
             type: "uuid",
-
           },
 
-
-
           {
-
             name: "user_id",
 
-
-
             type: "uuid",
-
           },
 
-
-
           {
-
             name: "start_date",
 
-
-
             type: "timestamp",
-
-
 
             default: "now()",
-
           },
 
           {
-
             name: "end_date",
 
-
-
             type: "timestamp",
-
           },
 
           {
-
             name: "end_date",
 
-
-
             type: "timestamp",
-
           },
           {
-
             name: "expected_return_date",
 
-
-
             type: "timestamp",
-
           },
           {
-
             name: "total",
 
-
-
             type: "numeric",
-
           },
           {
-
             name: "created_at",
 
-
-
             type: "timestamp",
             default: "now()",
-
           },
           {
-
             name: "updated_at",
-
-
 
             type: "timestamp",
             default: "now()",
-
           },
-
         ],
         foreignKeys: [
           {
@@ -138,8 +78,6 @@ export class CreateTableRentals1634265967650 implements MigrationInterface {
             onDelete: "SET NULL",
             onUpdate: "SET NULL",
           },
-        ],
-        foreignKeys: [
           {
             name: "FKUserRental",
             referencedTableName: "users",
@@ -149,18 +87,11 @@ export class CreateTableRentals1634265967650 implements MigrationInterface {
             onUpdate: "SET NULL",
           },
         ],
-
       })
-
     );
-
   }
-
-
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("rentals")
+    await queryRunner.dropTable("rentals");
   }
-
 }
-
